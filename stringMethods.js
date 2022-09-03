ac51a63480e190091503de0c02048c24464ac791
chithiraivel <chithiraivels11@gmail.com>
2022-09-03 22:39:50 +0530

    stringMethods.

---
+++ stringMethods.js
@@ -0,0 +1,115 @@
+
+//  1.charAt()
+
+// let a = 'javascript'
+// let b = a.charAt(4);
+// console.log(b);
+
+
+//  2.charCodeAt()
+
+// let a = 'jAva';
+// let b = a.charCodeAt(1)
+// console.log(b);
+
+
+//  3.concat()
+
+// var a ='java'
+// var b='javascript'
+// let c = a.concat(b)
+// console.log(c);
+
+
+//  4.endsWith()
+ 
+// var a = 'how are you'
+// var b = a.endsWith('u')
+// console.log(b);
+
+
+//  5.includes
+
+// var a = 'stands for GeeksforGeeks'
+// var b =a.includes('for')
+// console.log(b);
+
+
+
+//  6.indexOf()
+
+// var a = 'stands for GeeksforGeeks'
+// var b = a.indexOf('for')
+// console.log(b);
+
+
+//  7.repeat()
+
+// var a ='java'
+// var b = a.repeat(3)
+// console.log(b);
+
+
+//  8.replace()
+
+// var a = 'stands for GeeksforGeeks'
+// var b = a.replace('for' , 'is')
+// console.log(b);
+
+
+//  9.search
+
+// var a = 'stands for GeeksforGeeks'
+// var b = a.search('for')
+// console.log(b);
+
+
+//  10.slice
+
+// var a = 'stands for GeeksforGeeks'
+// var b = a.slice(1,4)
+// console.log(b);
+
+
+//  11.split()
+
+// var a = 'stands for GeeksforGeeks'
+// var b = a.split(' ')
+// console.log(b);
+
+
+//  12.startWith()
+
+// var a = 'java javascript python'
+// var b = a.startsWith('java')
+// console.log(b);
+
+
+//  13.toLowerCase()
+
+// var a = 'JAVASCRIPT'
+// var b= a.toLocaleLowerCase()
+// console.log(b);
+
+
+
+//  14.toUpperCase()
+
+// var a = 'python'
+// var b = a.toUpperCase()
+// console.log(b);
+
+
+// 15.trim()
+
+// var a = ' stands for GeeksforGeeks ';
+// var b =a.trim()
+// console.log(b);
+
+// 16.substr()
+
+// var a = 'python project'
+// var b = a.substring(1,3)
+// console.log(b);
+
+
